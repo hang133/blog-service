2bddb0c716375c88ef05d4f3dbe61cf81354a2be
Lihang <2680810339@qq.com>
2023-02-13 15:09:26 +0800

    update service

---
+++ internal/routers/router.go
@@ -0,0 +1,44 @@
+package routers
+
+import (
+	"blog-service/global"
+	"blog-service/internal/middleware"
+	"blog-service/internal/routers/api"
+	v1 "blog-service/internal/routers/api/v1"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"net/http"
+)
+
+func NewRouter() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.Use(middleware.Translations())
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	upload := api.NewUpload()
+	r.POST("/upload/file", upload.UploadFile)
+	r.StaticFS("/static", http.Dir(global.AppSetting.UploadSavePath))
+
+	r.POST("/auth", api.GetAuth)
+	article := v1.NewArticle()
+	tag := v1.NewTag()
+	apiv1 := r.Group("/api/v1")
+	apiv1.Use(middleware.JWT())
+	{
+		apiv1.POST("/tags", tag.Create)
+		apiv1.DELETE("/tags/:id", tag.Delete)
+		apiv1.PUT("/tags/:id", tag.Update)
+		apiv1.PATCH("/tags/:id/state", tag.Update)
+		apiv1.GET("/tags", tag.List)
+
+		apiv1.POST("/articles", article.Create)
+		apiv1.DELETE("/articles/:id", article.Delete)
+		apiv1.PUT("/articles/:id", article.Update)
+		apiv1.PATCH("/articles/:id/state", article.Update)
+		apiv1.GET("/articles/:id", article.Get)
+		apiv1.GET("/articles", article.List)
+	}
+	return r
+}
